c24e3ebb852ae467b0136890458ee4ff254040f8
Joshua Ob <joshuaaob@gmail.com>
2018-10-09 23:49:05 +0100

    add work list show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'work_lists/index'
+
+  get 'work_lists/show'
+
   devise_for :admin_users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
   resources :admin_users, only: :index
   resources :users, only: %i[index show]
   resources :companies, only: %i[index]
-  resources :work_lists, only: %i[index]
+  resources :work_lists, only: %i[index show]
   resources :reports, only: %i[index]
 
   get 'dashboard', to: 'dashboard#show'
