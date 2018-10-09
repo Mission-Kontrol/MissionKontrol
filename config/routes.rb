f28618a5788755a464b258605088d05c5c452f08
Joshua Ob <joshuaaob@gmail.com>
2018-10-09 16:27:17 +0100

    update routes for dashbaord sidenav links

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@
 
 Rails.application.routes.draw do
   devise_for :admin_users
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
   resources :admin_users, only: :index
-
   resources :users, only: %i[index show]
+  resources :companies, only: %i[index]
+  resources :work_lists, only: %i[index]
+  resources :reports, only: %i[index]
+
+  get 'dashboard', to: 'dashboard#show'
 end
