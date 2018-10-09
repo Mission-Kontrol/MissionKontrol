336645adc0d59805ad9b7cc11aa7edad34d68af7
Joshua Ob <joshuaaob@gmail.com>
2018-10-09 17:44:33 +0100

    use matcher insensitive to item order in array and add missing companies table

--- spec/unit/lib/kuwinda/use_case/database_connection_spec.rb
+++ spec/unit/lib/kuwinda/use_case/database_connection_spec.rb
@@ -6,11 +6,11 @@ describe Kuwinda::UseCase::DatabaseConnection do
   context 'when establishing a database connection' do
     subject { described_class.new.execute }
     let(:expected_result) do
-      %w[attending_events schema_migrations welcomes users events]
+      %w[attending_events schema_migrations welcomes users events companies]
     end
 
     it 'can list the tables available in the database' do
-      expect(subject.connection.tables).to eq expected_result
+      expect(subject.connection.tables).to match_array expected_result
     end
   end
 end
