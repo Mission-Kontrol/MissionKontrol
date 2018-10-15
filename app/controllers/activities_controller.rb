0d4fd79adc157ec2aff0020641b984e636bf7c1d
Joshua Ob <joshuaaob@gmail.com>
2018-10-15 19:01:30 +0100

    rubocopped

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,21 +1,21 @@
+# frozen_string_literal: true
+
 class ActivitiesController < ApplicationController
-  # POST /activities
-  # POST /activities.json
   def create
     @activity = Activity.new(activity_params)
 
     respond_to do |format|
       if @activity.save
-        format.js { render :action => "success" }
+        format.js { render action: 'success' }
       else
-        format.js { render :action => "failure", status: :unprocessable_entity }
+        format.js { render action: 'failure', status: :unprocessable_entity }
       end
     end
-  end 
+  end
 
   private
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def activity_params
-      params.require(:activity).permit(:user_id, :content, :kind)
-    end
+
+  def activity_params
+    params.require(:activity).permit(:user_id, :content, :kind)
+  end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,12 +2,10 @@
 
 class Activity < ApplicationRecord
   validates :content, presence: true
-  
+
   class << self
-
     def find_all_by_user_id(user_id:)
-      self.where(user_id: user_id)
+      where(user_id: user_id).order(created_at: :desc)
     end
-
   end
 end
