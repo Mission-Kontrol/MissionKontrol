4278727ad04b1ea990a260be66102696fc3cdc97
Joshua Ob <joshuaaob@gmail.com>
2018-10-18 21:14:45 +0100

    refactor long methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,26 +2,52 @@
 
 class UsersController < ApplicationController
   layout 'dashboard'
+  before_action :set_activities_for_user, only: :show
 
   def index
     @users = User.all
+    @companies = Company.all
   end
 
   def show
-    @activities = activities_for_user
-    @activity = Activity.new
     @user = User.find(params[:id])
+    @activity = @user.activities.new
+    @activities = OpenStruct.new
+    group_activities_by_kind
   end
 
   private
 
-  def activities_for_user
-    activities_for_user = Activity.find_all_by_user_id(user_id: params[:id])
-    activities = OpenStruct.new
-    activities.all = activities_for_user
-    activities.calls = activities_for_user.select { |i| i.kind == 'call' }
-    activities.notes = activities_for_user.select { |i| i.kind == 'note' }
-    activities.meetings = activities_for_user.select { |i| i.kind == 'meeting' }
-    activities
+  def set_activities_for_user
+    @activities_for_user = User.find(params[:id]).activities
+  end
+
+  def group_activities_by_kind
+    select_all_activities
+    select_call_activities
+    select_note_activities
+    select_meeting_activities
+  end
+
+  def select_all_activities
+    @activities.all = @activities_for_user
+  end
+
+  def select_call_activities
+    @activities.calls = @activities_for_user.select do |i|
+      i.kind == 'call'
+    end
+  end
+
+  def select_meeting_activities
+    @activities.meetings = @activities_for_user.select do |i|
+      i.kind == 'meeting'
+    end
+  end
+
+  def select_note_activities
+    @activities.notes = @activities_for_user.select do |i|
+      i.kind == 'note'
+    end
   end
 end
