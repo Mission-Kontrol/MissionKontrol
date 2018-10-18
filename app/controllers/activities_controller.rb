8fed65d8e0af8008dc84a436b9a6025ee4413e76
Joshua Ob <joshuaaob@gmail.com>
2018-10-18 21:13:54 +0100

    implement polymorphic activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 class ActivitiesController < ApplicationController
+  before_action :set_feedable, only: [:create]
+
   def create
-    @activity = Activity.new(activity_params)
+    @activity = @feedable.activities.new(activity_params)
 
     respond_to do |format|
-      if @activity.save
+      if @feedable.save
         format.js { render action: 'success' }
       else
         format.js { render action: 'failure', status: :unprocessable_entity }
@@ -16,6 +18,14 @@ class ActivitiesController < ApplicationController
   private
 
   def activity_params
-    params.require(:activity).permit(:user_id, :content, :kind)
+    params.require(:activity).permit(:content,
+                                     :kind,
+                                     :feedable_type,
+                                     :feedable_id)
+  end
+
+  def set_feedable
+    feedable_klass = params[:activity][:feedable_type].capitalize.constantize
+    @feedable = feedable_klass.find(params[:activity][:feedable_id])
   end
 end
