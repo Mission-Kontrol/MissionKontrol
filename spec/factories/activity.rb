ba08ab177085806852139964d07cfd8422802ca1
Joshua Ob <joshuaaob@gmail.com>
2018-10-18 21:21:01 +0100

    add trait for User

--- spec/factories/activity.rb
+++ spec/factories/activity.rb
@@ -4,5 +4,10 @@ FactoryBot.define do
   factory :activity do
     content 'met with a user today'
     kind 'meeting'
+
+    trait :user do
+      feedable_id 1
+      feedable_type 'User'
+    end
   end
 end
