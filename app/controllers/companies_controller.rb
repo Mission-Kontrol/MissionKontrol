e009fcbf657713cf90ec2113d756e09c889f06d4
Joshua Ob <joshuaaob@gmail.com>
2018-10-18 21:14:45 +0100

    refactor long methods

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,6 +2,7 @@
 
 class CompaniesController < ApplicationController
   layout 'dashboard'
+  before_action :set_activities_for_company, only: :show
 
   def index
     @companies = Company.all
@@ -9,5 +10,43 @@ class CompaniesController < ApplicationController
 
   def show
     @company = Company.find(params[:id])
+    @activities = OpenStruct.new
+    @activity = @company.activities.new
+    group_activities_by_kind
+  end
+
+  private
+
+  def set_activities_for_company
+    @activities_for_company = Company.find(params[:id]).activities
+  end
+
+  def group_activities_by_kind
+    select_all_activities
+    select_call_activities
+    select_note_activities
+    select_meeting_activities
+  end
+
+  def select_all_activities
+    @activities.all = @activities_for_company
+  end
+
+  def select_call_activities
+    @activities.calls = @activities_for_company.select do |i|
+      i.kind == 'call'
+    end
+  end
+
+  def select_meeting_activities
+    @activities.meetings = @activities_for_company.select do |i|
+      i.kind == 'meeting'
+    end
+  end
+
+  def select_note_activities
+    @activities.notes = @activities_for_company.select do |i|
+      i.kind == 'note'
+    end
   end
 end
