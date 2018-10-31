6087a64b8d4253a9339a958b83ed9245b95cf550
Joshua Ob <joshuaaob@gmail.com>
2018-10-31 16:07:55 +0000

    implement sensitive data for admin user

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,4 +5,86 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  include SensitiveData
+
+  def admin_database_name
+    SensitiveData.get_admin_database_credential(:database_name)
+  end
+
+  def admin_database_name=(name)
+    SensitiveData.set_admin_database_credential(:database_name, name)
+  end
+
+  def admin_database_username
+    SensitiveData.get_admin_database_credential(:database_username)
+  end
+
+  def admin_database_username=(username)
+    SensitiveData.set_admin_database_credential(:database_username, username)
+  end
+
+  def admin_database_password
+    SensitiveData.get_admin_database_credential(:database_password)
+  end
+
+  def admin_database_password=(password)
+    SensitiveData.set_admin_database_credential(:database_password, password)
+  end
+
+  def admin_database_host
+    SensitiveData.get_admin_database_credential(:database_host)
+  end
+
+  def admin_database_host=(host)
+    SensitiveData.set_admin_database_credential(:database_host, host)
+  end
+
+  def admin_database_port
+    SensitiveData.get_admin_database_credential(:database_port)
+  end
+
+  def admin_database_port=(port)
+    SensitiveData.set_admin_database_credential(:database_port, port)
+  end
+
+  def target_database_name
+    SensitiveData.get_target_database_credential(:database_name)
+  end
+
+  def target_database_name=(name)
+    SensitiveData.set_target_database_credential(:database_name, name)
+  end
+
+  def target_database_username
+    SensitiveData.get_target_database_credential(:database_username)
+  end
+
+  def target_database_username=(username)
+    SensitiveData.set_target_database_credential(:database_username, username)
+  end
+
+  def target_database_password
+    SensitiveData.get_target_database_credential(:database_password)
+  end
+
+  def target_database_password=(password)
+    SensitiveData.set_target_database_credential(:database_password, password)
+  end
+
+  def target_database_host
+    SensitiveData.get_target_database_credential(:database_host)
+  end
+
+  def target_database_host=(host)
+    SensitiveData.set_target_database_credential(:database_host, host)
+  end
+
+  def target_database_port
+    SensitiveData.get_target_database_credential(:database_port)
+  end
+
+  def target_database_port=(port)
+    SensitiveData.set_target_database_credential(:database_port, port)
+  end
 end
--- spec/unit/models/admin_user_spec.rb
+++ spec/unit/models/admin_user_spec.rb
@@ -2,10 +2,37 @@
 
 require 'rails_helper'
 
-describe AdminUser do
-  let(:an_admin_user) {described_class.new }
+def db_credentials
+  %w[
+    admin_database_name
+    admin_database_name=
+    admin_database_username
+    admin_database_username=
+    admin_database_password
+    admin_database_password=
+    admin_database_host
+    admin_database_host=
+    admin_database_port
+    admin_database_port=
+    target_database_name
+    target_database_name=
+    target_database_username
+    target_database_username=
+    target_database_password
+    target_database_password=
+    target_database_host
+    target_database_host=
+    target_database_port
+    target_database_port=
+  ]
+end
 
-  it "responds to #get_admin_database_name" do
-    expect(an_admin_user).to respond_to(:get_admin_database_name)
+describe AdminUser do
+  let(:an_admin_user) { described_class.new }
+
+  db_credentials.each do |credential|
+    it "responds to ##{credential}" do
+      expect(an_admin_user).to respond_to(credential.to_sym)
+    end
   end
 end
