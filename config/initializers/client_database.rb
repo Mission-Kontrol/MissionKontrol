636d4a1e09a56e02d7448afd68cda2b712205f19
Joshua Ob <joshuaaob@gmail.com>
2018-11-02 15:42:17 +0000

    use client db credentials set by admin user

--- config/initializers/client_database.rb
+++ config/initializers/client_database.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-uri = URI.parse(ENV['CLIENT_DATABASE_URL'])
 CLIENT_DB = {
   adapter: 'postgresql',
   encoding: 'unicode',
   pool: 2,
-  username: uri.user,
-  password: uri.password,
-  port: uri.port,
-  host: uri.host,
-  database: uri.path.from(1)
+  username: SensitiveData.get_target_database_credential(:database_username),
+  password: SensitiveData.get_target_database_credential(:database_password),
+  port: SensitiveData.get_target_database_credential(:database_port),
+  host: SensitiveData.get_target_database_credential(:database_host),
+  database: SensitiveData.get_target_database_credential(:database_name)
 }.freeze
