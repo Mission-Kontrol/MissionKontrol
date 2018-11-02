8d3fd7707f78c3a62e4e339736db797a52194422
Joshua Ob <joshuaaob@gmail.com>
2018-11-02 13:49:20 +0000

    add flash message and remove html required constraints

--- app/controllers/admin_user_registrations_controller.rb
+++ app/controllers/admin_user_registrations_controller.rb
@@ -10,13 +10,6 @@ class AdminUserRegistrationsController < Devise::RegistrationsController
     resource.update_without_password(params)
   end
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(
-      :account_update,
-      keys: permitted_params_list + permitted_target_db_params
-    )
-  end
-
   def permitted_admin_db_params
     %i[
       admin_database_name
@@ -35,6 +28,13 @@ class AdminUserRegistrationsController < Devise::RegistrationsController
       target_database_host
       target_database_port
     ]
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(
+      :account_update,
+      keys: permitted_admin_db_params + permitted_target_db_params
+    )
   end
 
   def after_update_path_for(_resource)
