650bf43445e6c8c6b015427545dbd262a67f4724
Joshua Ob <joshuaaob@gmail.com>
2018-11-02 15:52:34 +0000

    use default db credentials in test

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,8 +1,12 @@
+# frozen_string_literal: true
+
 module DeviseHelper
   def devise_error_messages!
     return '' if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    messages = resource.errors.full_messages.map do |msg|
+      content_tag(:li, msg)
+    end.join
     html = <<-HTML
     <div class="alert alert-error alert-block"> <button type="button"
     class="close" data-dismiss="alert">x</button>
