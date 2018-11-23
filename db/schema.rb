cf9e19a7a962de29ba8ae37076e929690002aba1
Joshua Ob <joshuaaob@gmail.com>
2018-11-23 15:33:55 +0000

    add visible columns to work lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181116183747) do
+ActiveRecord::Schema.define(version: 20181122170852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20181116183747) do
     t.string "sql_query"
     t.jsonb "outcomes", default: []
     t.string "data_table_name"
+    t.jsonb "visible_columns", default: []
   end
 
 end
