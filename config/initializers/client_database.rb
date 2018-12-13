bf06558d685b1c9b8784163961c62d11b8e74b36
Joshua Ob <joshuaaob@gmail.com>
2018-12-13 20:22:24 +0000

    fix: client db credentials WIP

--- config/initializers/client_database.rb
+++ config/initializers/client_database.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-if Rails.env == 'test' || Rails.env.development?
+# if Rails.env == 'test' || Rails.env.development?
   uri = URI.parse(ENV['CLIENT_DATABASE_URL'])
   CLIENT_DB = {
     adapter: 'postgresql',
@@ -12,15 +12,15 @@ if Rails.env == 'test' || Rails.env.development?
     host: uri.host,
     database: uri.path.from(1)
   }.freeze
-else
-  CLIENT_DB = {
-    adapter: 'postgresql',
-    encoding: 'unicode',
-    pool: 2,
-    username: SensitiveData.get_target_database_credential(:database_username),
-    password: SensitiveData.get_target_database_credential(:database_password),
-    port: SensitiveData.get_target_database_credential(:database_port),
-    host: SensitiveData.get_target_database_credential(:database_host),
-    database: SensitiveData.get_target_database_credential(:database_name)
-  }.freeze
-end
+# else
+#   CLIENT_DB = {
+#     adapter: 'postgresql',
+#     encoding: 'unicode',
+#     pool: 2,
+#     username: SensitiveData.get_target_database_credential(:database_username),
+#     password: SensitiveData.get_target_database_credential(:database_password),
+#     port: SensitiveData.get_target_database_credential(:database_port),
+#     host: SensitiveData.get_target_database_credential(:database_host),
+#     database: SensitiveData.get_target_database_credential(:database_name)
+#   }.freeze
+# end
