bb3cdfac3d9f9fc4c0dff3952abda0796e5db7c9
Joshua Ob <joshuaaob@gmail.com>
2019-01-04 21:21:45 +0000

    Add JS to control field settings side nav

--- app/assets/javascripts/layout_builder.js
+++ app/assets/javascripts/layout_builder.js
@@ -9,6 +9,20 @@ $(document).ready(function() {
     $(tab).removeClass('hide');
   })
 
+  $('.layout_builder_selected_table_name').click(function(evt) {
+    evt.preventDefault();
+    var table = $(this).data().tableName;
+    console.log("selected table is - ", table);
+    hideFieldSettingsFormScreen1();
+    showFieldSettingsFormScreen2();
+    getOptionsForDraggable(table);
+  })
+
+  $('.layout_builder_field_settings_form_back_btn').click(function() {
+    hideFieldSettingsFormScreen1();
+    showFieldSettingsFormScreen2();
+  })
+
   $('#layout-builder-modal-next-button').click(function() {
     goToNextScreen();
   })
@@ -39,4 +53,38 @@ function goToNextScreen() {
 function goToPreviousScreen() {
   $('#screen-1').toggleClass('hide');
   $('#screen-2').toggleClass('hide');
+}
+
+function hideFieldSettingsFormScreen1() {
+  $('#layout_builder_field_settings_form_screen_1').toggleClass('hide');
+}
+
+function showFieldSettingsFormScreen2() {
+  $('#layout_builder_field_settings_form_screen_2').toggleClass('hide');
+}
+
+function getOptionsForDraggable(primaryTable) {
+  $.ajax({
+    url: "/layouts/table_fields_with_type",
+    type: 'GET',
+    data: {
+      table: primaryTable
+    },
+    async: true,
+    dataType: "json",
+    error: function(XMLHttpRequest, errorTextStatus, error){
+              alert("Failed: "+ errorTextStatus+" ;"+error);
+           },
+    success: function(data){
+      updateDraggable(data);
+    }
+  })
+}
+
+function updateDraggable(data) {
+  console.log(data);
+
+  // <div class="layout-builder-draggable-field">
+  //   <i class="<%= icon_for_field_type('string') %>" aria-hidden="true"></i> Name
+  // </div>
 }
