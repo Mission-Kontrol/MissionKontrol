fd2127fb957691f7981ccb88f094b39b4254f525
Joshua Ob <joshuaaob@gmail.com>
2019-01-13 02:56:45 +0000

    Remove comment

--- app/controllers/layout_builder_controller.rb
+++ app/controllers/layout_builder_controller.rb
@@ -36,14 +36,9 @@ class LayoutBuilderController < ApplicationController
     if @view_builder.save!
       render json: @view_builder
     end
-
-    # if @view_builder.save!
-    #   render 'configure_table_order', view_builder: @view_builder
-    # end
   end
 
   def update
-    # respond_to :js
     @view_builder = ViewBuilder.find(params[:id])
 
     update_attributes(@view_builder, params)
@@ -65,7 +60,6 @@ class LayoutBuilderController < ApplicationController
   def view_page; end
 
   def edit
-    # @view_builder = ViewBuilder.find(params[:id])
     @available_tables = available_tables
     @layout_setting = LayoutSetting.find_by_layout_id(@view_builder.id) || LayoutSetting.new
   end
@@ -119,19 +113,11 @@ class LayoutBuilderController < ApplicationController
   end
 
   def update_attributes(view_builder, params)
-    # view_builder.table_attributes = configure_attributes(params[:tableConfigurations])
-    # view_builder.table_attributes[:default_rows] = params[:defaultRows]
     view_builder.status = params[:status] if params[:status]
     view_builder.view_name = params[:name] if params[:name]
     view_builder.commentable = params[:view_builder][:commentable] if params[:view_builder][:commentable]
     view_builder.show_status = params[:view_builder][:show_status] if params[:view_builder][:show_status]
     view_builder.table_name = params[:view_builder][:table_name] if params[:view_builder][:table_name]
     view_builder.parent_comment_table = params[:view_builder][:parent_comment_table] if params[:view_builder][:parent_comment_table]
-    # binding.pry
   end
-
-  # def handle_success(action:, js_func:, notice:)
-  #   flash[:notice] = notice
-  #   render(action: action, js: js_func)
-  # end
 end
