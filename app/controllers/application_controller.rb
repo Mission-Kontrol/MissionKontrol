0d5559e5f7cecbbf4847c10012b446bb8a79ee19
Joshua Ob <joshuaaob@gmail.com>
2019-01-22 13:33:56 +0000

    Redirect to layout builder on sign up not sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def after_sign_in_path_for(resource)
+  def after_sign_up_path_for(resource)
     new_layout_path
   end
 end
