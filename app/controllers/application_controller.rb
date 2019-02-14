84c28696acb45cae7d32c494b87dcce0de9e22ad
Joshua Ob <joshuaaob@gmail.com>
2019-02-14 15:59:11 +0000

    Prevent loading available tables for every controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,17 +4,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :load_view_builders,
-                :load_target_db_tables
+  before_action :load_view_builders
 
   private
 
   def load_view_builders
     @view_builders = ViewBuilder.where(status: 'active')
-  end
-
-  def load_target_db_tables
-    @target_db_tables ||= Kuwinda::Presenter::ListAvailableTables.new(ClientRecord).call
   end
 
   protected
