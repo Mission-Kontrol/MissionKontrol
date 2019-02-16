fb53d76d59f7446f1ff840d2964534239ab42ece
Joshua Ob <joshuaaob@gmail.com>
2019-02-16 02:53:54 +0000

    Reduce scope of exception handling

--- lib/kuwinda/presenter/list_available_tables.rb
+++ lib/kuwinda/presenter/list_available_tables.rb
@@ -3,7 +3,7 @@
 module Kuwinda
   module Presenter
     class ListAvailableTables
-  
+
       def initialize(database)
         @database = database
       end
@@ -11,10 +11,6 @@ module Kuwinda
       def call
         tables = database.connection.tables - ['schema_migrations']
         tables.map
-      rescue Mysql2::Error => e
-        []
-      rescue PG::ConnectionBad => e
-        []
       end
 
       private
