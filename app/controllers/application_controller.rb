ea9cfcbea7c77fe10e40425bd48bfaef101fc48d
Joshua Ob <joshuaaob@gmail.com>
2019-02-16 02:49:49 +0000

    Handle invalid client db error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,12 +4,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :load_view_builders
+  before_action :load_view_builders,
+                :load_available_tables
 
   private
 
   def load_view_builders
     @view_builders = ViewBuilder.where(status: 'active')
+  end
+
+  def load_available_tables
+    @available_tables = Kuwinda::Presenter::ListAvailableTables.new(ClientRecord).call
+  rescue Kuwinda::Gateway::InvalidClientDatabaseError => e
+    @available_tables = []
   end
 
   protected
