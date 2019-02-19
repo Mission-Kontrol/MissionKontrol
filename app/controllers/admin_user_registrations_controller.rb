0f5d90e76e8a4350ce23358179a335b0ba914b0d
Camilla van Klinken <camillavk@gmail.com>
2019-02-19 05:55:18 +0000

    WIP saving target database credentials
    Aside from the demo requirements, some of the constraints won't be an issue when it is self hosted:
    When starting up for the first time when the user registers, it will not try and connect to the target database until they have registered the target database (they should not be able to get to the dashboard until a successful connection has been made)
    
    I _think_ the issues you were encountering with the inconsistency of the target database were because the credentials weren't saving correctly? The fix I have put in is very ugly and needs some tidying up/making prettier but the general logic/concept is there. I don't think Devise liked having the first_name and last_name fields saved differently from the target_database_* fields.
    
    Let me know if this change makes any difference to you. Currently, as we are saving the credentials with the SensitiveData module, the target database is not saved against a user so if you want to switch database type (i.e. from mysql to postgres) then you need to re-register with the new database. This functionality is frustrating for now but is the correct functionality for the final application. As it will be self hosted, there should not be a need to change databases regularly. The ability to have multiple database dashboards will be in version two or three :D

--- app/controllers/admin_user_registrations_controller.rb
+++ app/controllers/admin_user_registrations_controller.rb
@@ -4,7 +4,23 @@ class AdminUserRegistrationsController < Devise::RegistrationsController
   before_action :configure_permitted_parameters, if: :devise_controller?
   layout 'application', only: [:new]
 
+  after_action :set_target_database_credentials, only: :create
+
   protected
+
+  def set_target_database_credentials
+    admin_user = AdminUser.find_by(email: admin_user_params[:email])
+    admin_user.first_name = admin_user_params[:first_name]
+    admin_user.last_name = admin_user_params[:last_name]
+    admin_user.company_name = admin_user_params[:company_name]
+    admin_user.target_database_type = admin_user_params[:target_database_type]
+    admin_user.target_database_name = admin_user_params[:target_database_name]
+    admin_user.target_database_username = admin_user_params[:target_database_username]
+    admin_user.target_database_password = admin_user_params[:target_database_password]
+    admin_user.target_database_host = admin_user_params[:target_database_host]
+    admin_user.target_database_port = admin_user_params[:target_database_port]
+    admin_user.save!
+  end
 
   def update_resource(resource, params)
     resource.update_without_password(params)
@@ -13,41 +29,56 @@ class AdminUserRegistrationsController < Devise::RegistrationsController
     resource
   end
 
-  def permitted_admin_db_params
-    %i[
-      admin_database_name
-      admin_database_username
-      admin_database_password
-      admin_database_host
-      admin_database_port
-      admin_database_type
-    ]
-  end
-
-  def permitted_target_db_params
-    %i[
-      target_database_name
-      target_database_username
-      target_database_password
-      target_database_host
-      target_database_port
-      target_database_type
-    ]
-  end
+  # def permitted_admin_db_params
+  #   %i[
+  #     admin_database_name
+  #     admin_database_username
+  #     admin_database_password
+  #     admin_database_host
+  #     admin_database_port
+  #     admin_database_type
+  #   ]
+  # end
+  #
+  # def permitted_target_db_params
+  #   %i[
+  #     target_database_name
+  #     target_database_username
+  #     target_database_password
+  #     target_database_host
+  #     target_database_port
+  #     target_database_type
+  #   ]
+  # end
 
   def configure_permitted_parameters
-    db_params = permitted_admin_db_params + permitted_target_db_params
-    devise_parameter_sanitizer.permit(
-      :account_update,
-      keys: %w[
-        first_name
-        last_name
-        company_name
-      ] + permitted_admin_db_params + permitted_target_db_params
-    )
+    # db_params = permitted_admin_db_params + permitted_target_db_params
+    # devise_parameter_sanitizer.permit(
+    #   :account_update,
+    #   keys: %w[
+    #     first_name
+    #     last_name
+    #     company_name
+    #   ] + permitted_admin_db_params + permitted_target_db_params
+    # )
+    # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(admin_user: [:first_name, :last_name]) }
+    # devise_parameter_sanitizer.permit(:sign_up, keys: [admin_user: [:first_name, :last_name, :company_name]])
   end
 
   def after_update_path_for(_resource)
     dashboard_path
+  end
+
+  def admin_user_params
+    params.require(:admin_user).permit(:email,
+                                       :first_name,
+                                       :last_name,
+                                       :company_name,
+                                       :target_database_type,
+                                       :target_database_name,
+                                       :target_database_username,
+                                       :target_database_password,
+                                       :target_database_host,
+                                       :target_database_port)
   end
 end
