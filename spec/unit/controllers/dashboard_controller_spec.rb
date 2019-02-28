13393cfb907e0e7685de85f4a65499426e253172
Joshua Ob <joshuaaob@gmail.com>
2019-02-28 13:08:15 +0000

    Add dashboard controller spec

---
+++ spec/unit/controllers/dashboard_controller_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe DashboardController, :type => :controller do
+  let(:admin) do
+    AdminUser.first_or_create(email: 'test@test.com', password: '123456', password_confirmation: '123456')
+  end
+
+  describe 'GET show' do
+    context "when client database connection is invalid" do
+      it "renders the bad connection template" do
+        sign_in admin
+        allow(controller).to receive(:show).and_raise(InvalidClientDatabaseError.new)
+        get :show
+
+        expect(response).to render_template("tables/bad_connection")
+      end
+    end
+
+    context "when client database connection is valid" do
+      it "renders the show template" do
+        sign_in admin
+        get :show
+        expect(response).to render_template("show")
+      end
+    end
+  end
+end
