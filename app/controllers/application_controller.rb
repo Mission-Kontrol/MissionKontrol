2cf4cfdefd099bb6f4bdd7c8f5678e21b8b06227
Joshua Ob <joshuaaob@gmail.com>
2019-02-28 13:02:26 +0000

    Add global handler for invalid client db error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
+class InvalidClientDatabaseError < StandardError; end
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :load_view_builders
+  rescue_from InvalidClientDatabaseError, :with => :handle_invalid_client_db_error
 
   private
 
@@ -16,5 +19,10 @@ class ApplicationController < ActionController::Base
 
   def after_sign_up_path_for(resource)
     new_layout_path
+  end
+
+  def handle_invalid_client_db_error
+    @available_tables = []
+    render '/tables/bad_connection'
   end
 end
