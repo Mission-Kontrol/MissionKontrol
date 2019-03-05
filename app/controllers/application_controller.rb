b446ef56353d37b1927e0204901b631fa2b9fb62
Joshua Ob <joshuaaob@gmail.com>
2019-03-05 13:40:11 +0000

    Test target db connection after log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,42 @@ class ApplicationController < ActionController::Base
     new_layout_path
   end
 
+  def after_sign_in_path_for(user)
+    test_target_db_connection
+    dashboard_path
+    
+  rescue Mysql2::Error,
+         PG::ConnectionBad,
+         InvalidClientDatabaseError => e
+
+   flash.discard
+   flash[:error] = "Invalid target database, please review credentials."
+   dashboard_path
+  end
+
+  private
+
+  def test_target_db_connection
+    ActiveRecord::Base.establish_connection(
+      :adapter  => adapter(current_admin_user.target_database_type),
+      :host     => current_admin_user.target_database_host,
+      :username => current_admin_user.target_database_username,
+      :password => current_admin_user.target_database_password,
+      :database => current_admin_user.target_database_name
+    ).connection
+  end
+
+  def adapter(scheme)
+   case scheme
+   when 'postgresql', 'postgres'
+     return 'postgresql'
+   when 'mysql', 'mysql2'
+     return 'mysql2'
+   else
+     raise InvalidClientDatabaseError.new("don't know how to make adpater for #{scheme}")
+   end
+  end
+
   def handle_invalid_client_db_error
     @available_tables = []
     render '/tables/bad_connection'
