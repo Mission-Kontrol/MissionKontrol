0b1044f8065ccc64458d9403d928cf26362f69f1
Joshua Ob <joshuaaob@gmail.com>
2019-03-08 21:29:14 +0000

    Update scheduler to run every minute to test out app reset

--- clock.rb
+++ clock.rb
@@ -5,4 +5,5 @@ handler do |job|
   puts "Running #{job}"
 end
 
-every(3600, 'bundle exec rake dummy_client_database:reset')
+every(60, 'bundle exec rake dummy_client_database:reset')
+# every(3600, 'bundle exec rake dummy_client_database:reset')
