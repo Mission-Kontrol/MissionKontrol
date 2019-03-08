21b71f9a5e98063aef84b4397151f8a15f8b300f
Joshua Ob <joshuaaob@gmail.com>
2019-03-08 14:23:11 +0000

    Hanlde invalid db credentials in demo

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -7,7 +7,22 @@ class DashboardController < ApplicationController
 
 
   def show
-    test_target_db_connection
+    if ENV['APP_ENV'] == 'demo'
+      begin
+        test_target_db_connection
+      rescue Mysql2::Error,
+             PG::ConnectionBad,
+             InvalidClientDatabaseError => e
+       uri = URI.parse(ENV['DEMO_DATABASE_PG'])
+       current_admin_user.target_database_host = uri.host
+       current_admin_user.target_database_name = uri.path.from(1)
+       current_admin_user.target_database_username = uri.user
+       current_admin_user.target_database_password = uri.password
+       current_admin_user.target_database_port = uri.port
+       current_admin_user.target_database_type = 'postgres'
+       @available_tables = Kuwinda::Presenter::ListAvailableTables.new(ClientRecord).call
+      end
+    end
   end
 
   private
