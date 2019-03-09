22cced70d066ad4695b38fc4e055086e908d1045
Joshua Ob <joshuaaob@gmail.com>
2019-03-09 18:01:28 +0000

    Run clock work every hour

--- clock.rb
+++ clock.rb
@@ -5,5 +5,4 @@ handler do |job|
   puts "Running #{job}"
 end
 
-every(60, 'bundle exec rake dummy_client_database:reset')
-# every(3600, 'bundle exec rake dummy_client_database:reset')
+every(3600, 'bundle exec rake dummy_client_database:reset')
