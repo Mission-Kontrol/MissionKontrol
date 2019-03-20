7542586d648767fcf1fa3fb7f6d712037e61f00a
Joshua Ob <joshuaaob@gmail.com>
2019-03-20 18:41:17 +0000

    Manage twilio api key via sensitive data module

--- app/models/concerns/sensitive_data.rb
+++ app/models/concerns/sensitive_data.rb
@@ -3,6 +3,16 @@
 module SensitiveData
   include ActiveSupport::Concern
 
+  def self.get_twilio_api_key
+    return twilio_api_key if twilio_api_key
+
+    nil
+  end
+
+  def self.set_twilio_api_key(key)
+    SensitiveData.save_twilio_api_key(key)
+  end
+
   def self.get_admin_database_credential(credential)
     return admin_db_credentials[credential] if admin_db_credentials
 
@@ -41,11 +51,18 @@ module SensitiveData
   private_class_method
 
   def self.admin_db_credentials_file_path
-    "./config/admin_db_credentials_#{Rails.env}.txt"
+    verify_sensitive_data_location
+    "./config/sensitive_data/admin_db_credentials_#{Rails.env}.txt"
   end
 
   def self.target_db_credentials_file_path
-    "./config/target_db_credentials_#{Rails.env}.txt"
+    verify_sensitive_data_location
+    "./config/sensitive_data/target_db_credentials_#{Rails.env}.txt"
+  end
+
+  def self.twilio_api_key_file_path
+    verify_sensitive_data_location
+    "./config/sensitive_data/twilio_api_key_#{Rails.env}.txt"
   end
 
   def self.save_admin_db_credentials(admin_db_credentials)
@@ -57,6 +74,12 @@ module SensitiveData
   def self.save_target_db_credentials(target_db_credentials)
     token = SensitiveData.encrypt(target_db_credentials)
     File.write(SensitiveData.target_db_credentials_file_path, token)
+    true
+  end
+
+  def self.save_twilio_api_key(key)
+    token = SensitiveData.encrypt(key)
+    File.write(SensitiveData.twilio_api_key_file_path, token)
     true
   end
 
@@ -78,9 +101,22 @@ module SensitiveData
     nil
   end
 
+  def self.twilio_api_key
+    if File.exist?(SensitiveData.twilio_api_key_file_path)
+      data = File.read(SensitiveData.twilio_api_key_file_path)
+      return SensitiveData.decrypt(data) unless data.blank?
+    end
+
+    nil
+  end
+
   def self.crypt(salt, password = Rails.application.secrets.secret_key_base)
     len = ActiveSupport::MessageEncryptor.key_len
     key = ActiveSupport::KeyGenerator.new(password).generate_key(salt, len)
     ActiveSupport::MessageEncryptor.new key
+  end
+
+  def self.verify_sensitive_data_location
+    FileUtils.mkdir_p './config/sensitive_data'
   end
 end
