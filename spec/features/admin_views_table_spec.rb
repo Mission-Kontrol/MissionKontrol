0a5bbf2dbe9feb1e67cbd70c3680a5c2c4f8b403
Joshua Ob <joshuaaob@gmail.com>
2019-03-22 09:50:38 +0000

    Add spec for admin viewing a table

--- spec/features/admin_views_table_spec.rb
+++ spec/features/admin_views_table_spec.rb
@@ -1,27 +1,15 @@
 require 'rails_helper'
 
 RSpec.feature 'Admin views a table', type: :feature do
-  let(:admin) do
-    AdminUser.first_or_create(email: 'test@test.com', password: '123456', password_confirmation: '123456')
-  end
-
   before do
     delete_layout_for_users
   end
 
-  scenario 'navigating to a table will show the selected table' do
-    log_in_as_admin
+  scenario 'clicking on a table will show the selected table' do
+    sign_in_as_admin
     when_i_click_the_users_link
     then_i_expect_to_be_redirected_to_the_users_table
   end
-end
-
-def log_in_as_admin
-  visit root_path
-	email = "test@test.com"
-	fill_in 'admin_user_email', :with => email
-	fill_in 'admin_user_password', :with => "123456"
-	click_button 'Log in'
 end
 
 def delete_layout_for_users
