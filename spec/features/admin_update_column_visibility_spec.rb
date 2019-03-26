dda993355bb1d640e2e12fdd3aeeb7a107950268
Joshua Ob <joshuaaob@gmail.com>
2019-03-26 18:44:05 +0000

    Remove redundant spec

--- spec/features/admin_update_column_visibility_spec.rb
+++
@@ -1,9 +0,0 @@
-# require 'rails_helper'
-#
-# RSpec.feature 'Admin settings page', type: :feature do
-#   xscenario 'navigating to users will redirect to users route' do
-#     given_i_am_on_the_dashboard_page
-#     when_i_click_the_users_link
-#     then_i_expect_to_be_redirected_to_the_users
-#   end
-# end
