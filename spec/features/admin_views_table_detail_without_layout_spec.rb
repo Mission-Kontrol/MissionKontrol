7c5c47a29599da94f2aee77aaa5edbab0384da3f
Joshua Ob <joshuaaob@gmail.com>
2019-03-26 18:44:22 +0000

    Add test for admin viewing a table record - WIP

--- spec/features/admin_views_table_detail_without_layout_spec.rb
+++ spec/features/admin_views_table_detail_without_layout_spec.rb
@@ -1,16 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature 'Admin views a table without a layout', type: :feature, js: true do
-  let(:admin) do
-    AdminUser.first_or_create(email: 'test@test.com', password: '123456', password_confirmation: '123456')
-  end
-
+RSpec.feature 'Admin views a table without a layout', type: :feature do
   before do
     delete_layout_for_users
   end
 
   scenario 'navigating to a table item without a layout will redirect to the layout builder' do
-    log_in_as_admin
+    sign_in_as_admin
     then_click_the_users_link
     when_i_click_the_first_user
     then_i_expect_to_be_redirected_to_the_layout_builder
@@ -21,24 +17,18 @@ def delete_layout_for_users
   ViewBuilder.where(table_name: "users").delete_all
 end
 
-def log_in_as_admin
-  visit root_path
-	email = "test@test.com"
-	fill_in 'admin_user_email', :with => email
-	fill_in 'admin_user_password', :with => "123456"
-	click_button 'Log in'
-end
-
 def then_click_the_users_link
-  click_link 'users'
-
-  # within('#side-menu') do
-  # end
+  within('#side-menu') do
+    click_link 'users'
+  end
 end
 
 def when_i_click_the_first_user
   within('.table') do
-    first('tbody > tr').click
+    # page.find(:css, ".clickable-row").click()
+    # first('tbody > tr').click
+    find('tr[data-href="/tables/users/3?table=users"]').click
+    # page.find(:xpath, "//table/tbody/tr").first.click
   end
 end
 
