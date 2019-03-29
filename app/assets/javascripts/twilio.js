713b7e51a26ca7595f1aed091226692eb8e27d7b
Joshua Ob <joshuaaob@gmail.com>
2019-03-29 14:18:07 +0000

    Add twilio callbacks for handling a browser call

--- app/assets/javascripts/twilio.js
+++ app/assets/javascripts/twilio.js
@@ -1,15 +1,33 @@
-
 /* Get a Twilio Client token with an AJAX request */
 $(document).ready(function() {
   $.post("/token/generate", {page: window.location.pathname}, function(data) {
     // Set up the Twilio Client Device with the token
     Twilio.Device.setup(data.token);
   });
-});
 
-/* Callback to let us know Twilio Client is ready */
-Twilio.Device.ready(function (device) {
-  updateCallStatus("Ready");
+  /* Report any errors to the call status display */
+  Twilio.Device.error(function (error) {
+    updateCallStatus("ERROR: " + error.message);
+  });
+
+  /* Callback to let us know Twilio Client is ready */
+  Twilio.Device.ready(function (device) {
+    updateCallStatus("Ready");
+  });
+
+  /* Callback for when Twilio Client initiates a new connection */
+  Twilio.Device.connect(function (connection) {
+    $("#call-status").removeClass('hide');
+    $(".hangup-button").prop("disabled", false);
+  });
+
+  /* Callback for when a call ends */
+  Twilio.Device.disconnect(function(connection) {
+    $("#call-status").addClass('hide');
+    $(".hangup-button").prop("disabled", true);
+    $(".call-customer-button").prop("disabled", false);
+    updateCallStatus("Ready");
+  });
 });
 
 /* Call a customer from a support ticket */
@@ -20,21 +38,12 @@ function callCustomer(phoneNumber) {
   Twilio.Device.connect(params);
 }
 
+/* Helper function to update the call status bar */
+function updateCallStatus(status) {
+  $("#call-status").text(status);
+}
 
-/* Callback for when Twilio Client initiates a new connection */
-Twilio.Device.connect(function (connection) {
-  // Enable the hang up button and disable the call buttons
-  hangUpButton.prop("disabled", false);
-  callCustomerButtons.prop("disabled", true);
-  callSupportButton.prop("disabled", true);
-  answerButton.prop("disabled", true);
-
-  // If phoneNumber is part of the connection, this is a call from a
-  // support agent to a customer's phone
-  if ("phoneNumber" in connection.message) {
-    updateCallStatus("In call with " + connection.message.phoneNumber);
-  } else {
-    // This is a call from a website user to a support agent
-    updateCallStatus("In call with support");
-  }
-});
+/* End a call */
+function hangUp() {
+  Twilio.Device.disconnectAll();
+}
