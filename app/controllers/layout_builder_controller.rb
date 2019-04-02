47c5ad0a708406ec976b9559863f68e4420c42b0
Joshua Ob <joshuaaob@gmail.com>
2019-04-02 13:21:22 +0100

    Improve params filter for updates to layout

--- app/controllers/layout_builder_controller.rb
+++ app/controllers/layout_builder_controller.rb
@@ -101,16 +101,19 @@ class LayoutBuilderController < ApplicationController
   def update_attributes(view_builder, params)
     view_builder.status = params[:status] if params[:status]
     view_builder.view_name = params[:name] if params[:name]
-    view_builder.commentable = params[:view_builder][:commentable] if params[:view_builder][:commentable]
-    view_builder.show_status = params[:view_builder][:show_status] if params[:view_builder][:show_status]
-    view_builder.table_name = params[:view_builder][:table_name] if params[:view_builder][:table_name]
-    view_builder.parent_comment_table = params[:view_builder][:parent_comment_table] if params[:view_builder][:parent_comment_table]
-    view_builder.draggable_fields_header_container1 = params[:view_builder][:draggable_fields_header_container1] if params[:view_builder][:draggable_fields_header_container1]
-    view_builder.draggable_fields_header_container2 = params[:view_builder][:draggable_fields_header_container2] if params[:view_builder][:draggable_fields_header_container2]
-    view_builder.draggable_fields_side_container = params[:view_builder][:draggable_fields_side_container] if params[:view_builder][:draggable_fields_side_container]
-    view_builder.draggable_fields_main_container1 = params[:view_builder][:draggable_fields_main_container1] if params[:view_builder][:draggable_fields_main_container1]
-    view_builder.draggable_fields_main_container2 = params[:view_builder][:draggable_fields_main_container2] if params[:view_builder][:draggable_fields_main_container2]
-    view_builder.draggable_fields_main_container3 = params[:view_builder][:draggable_fields_main_container3] if params[:view_builder][:draggable_fields_main_container3]
-    view_builder.hidden_columns = params[:view_builder][:hidden_columns] if params[:view_builder][:hidden_columns]
+
+    if params[:view_builder]
+      view_builder.commentable = params[:view_builder][:commentable] if params[:view_builder][:commentable]
+      view_builder.show_status = params[:view_builder][:show_status] if params[:view_builder][:show_status]
+      view_builder.table_name = params[:view_builder][:table_name] if params[:view_builder][:table_name]
+      view_builder.parent_comment_table = params[:view_builder][:parent_comment_table] if params[:view_builder][:parent_comment_table]
+      view_builder.draggable_fields_header_container1 = params[:view_builder][:draggable_fields_header_container1] if params[:view_builder][:draggable_fields_header_container1]
+      view_builder.draggable_fields_header_container2 = params[:view_builder][:draggable_fields_header_container2] if params[:view_builder][:draggable_fields_header_container2]
+      view_builder.draggable_fields_side_container = params[:view_builder][:draggable_fields_side_container] if params[:view_builder][:draggable_fields_side_container]
+      view_builder.draggable_fields_main_container1 = params[:view_builder][:draggable_fields_main_container1] if params[:view_builder][:draggable_fields_main_container1]
+      view_builder.draggable_fields_main_container2 = params[:view_builder][:draggable_fields_main_container2] if params[:view_builder][:draggable_fields_main_container2]
+      view_builder.draggable_fields_main_container3 = params[:view_builder][:draggable_fields_main_container3] if params[:view_builder][:draggable_fields_main_container3]
+      view_builder.hidden_columns = params[:view_builder][:hidden_columns] if params[:view_builder][:hidden_columns]
+    end
   end
 end
