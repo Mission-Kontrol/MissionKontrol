bcbc49f02ed529592e3eae8bc98d3bf5ad1a7936
Joshua Ob <joshuaaob@gmail.com>
2019-04-03 08:11:58 +0100

    Fetch twilio credentials from current admin user

--- app/controllers/call_controller.rb
+++ app/controllers/call_controller.rb
@@ -8,16 +8,11 @@ class CallController < ApplicationController
   private
 
   def twilio_reponse
-    twilio_number = ENV['TWILIO_PHONE_NUMBER']
+    admin = AdminUser.last
 
     res = Twilio::TwiML::VoiceResponse.new do |response|
-      dial = Twilio::TwiML::Dial.new caller_id: twilio_number
-
-      if params.include?(:phoneNumber)
-        dial.number params[:phoneNumber]
-      else
-        dial.client(identity: 'support_agent')
-      end
+      dial = Twilio::TwiML::Dial.new caller_id: admin.twilio_caller_id
+      dial.number params[:phoneNumber]
       response.append(dial)
     end
     return res.to_s
