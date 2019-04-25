f61c27c01f99d4d82bb04a379f873be12bf5fc2f
Joshua Ob <joshuaaob@gmail.com>
2019-04-25 18:32:04 +0100

    Add query_builer_rules to task_queues

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190424163632) do
+ActiveRecord::Schema.define(version: 20190425152656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20190424163632) do
     t.string "table"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.jsonb "query_builder_rules", default: []
   end
 
   create_table "view_builders", force: :cascade do |t|
