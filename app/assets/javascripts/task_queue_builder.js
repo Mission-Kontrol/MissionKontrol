58843929ed1c62c45ea583ef05c15cd752e70ad4
Joshua Ob <joshuaaob@gmail.com>
2019-04-26 16:40:51 +0100

    Reload task queue preview after query builder update

--- app/assets/javascripts/task_queue_builder.js
+++ app/assets/javascripts/task_queue_builder.js
@@ -1,14 +1,26 @@
-var queryBuilderFilters;
+let queryBuilderFilters;
+let metaTag;
+let isCurrentControllerTaskQueues;
+let isCurrentActionIndex;
+let isCurrentActionEdit;
+
+
 
 $(document).ready(() => {
-  let metaTag = $('meta[name=psj]');
-  let isCurrentControllerQueues = metaTag.attr('controller') == 'task_queues';
-  let isCurrentActionIndex = metaTag.attr('action') == 'index';
+  metaTag = $('meta[name=psj]');
+  isCurrentControllerTaskQueues = metaTag.attr('controller') == 'task_queues';
+  isCurrentActionIndex = metaTag.attr('action') == 'index';
+  isCurrentActionEdit = metaTag.attr('action') == 'edit';
 
-  if (isCurrentControllerQueues && isCurrentActionIndex) {
-    $('#new-queue-modal').modal({
-      // backdrop: 'static',
-      // keyboard: false
+  loadIndexPage();
+  loadEditPage();
+})
+
+function loadIndexPage() {
+  if (isCurrentControllerTaskQueues && isCurrentActionIndex) {
+    $('#new-task-queue-modal').modal({
+      backdrop: 'static',
+      keyboard: false
     })
 
     getFieldsWithType("users");
@@ -30,9 +42,65 @@ $(document).ready(() => {
       $('#new-queue-modal-screen-1').toggleClass('hide');
       $('#new-queue-modal-screen-2').toggleClass('hide');
     })
-  }
-})
 
+    $('#queue-builder-modal-save-button').click(function() {
+      var params = {};
+      params["task_queue"] = {}
+      params["task_queue"]["name"] = document.getElementById('task_queue_name').value;
+      params["task_queue"]["details"] = document.getElementById('task_queue_details').value;
+      params["task_queue"]["table"] = document.getElementById('task_queue_table').value;
+      saveTaskQueue(params);
+    })
+  }
+}
+
+function loadEditPage() {
+  if (isCurrentControllerTaskQueues && isCurrentActionEdit) {
+    let taskQueueId = document.getElementById("builder").dataset.taskQueueId;
+    let taskQueueTable = document.getElementById("builder").dataset.taskQueueTable;
+
+
+    // if (!$.isEmptyObject(queryBuilderRules)) {
+      // $('.qb-rules').val(JSON.stringify(queryBuilderRules, null, 2));
+    // }
+
+    getFieldsWithType(taskQueueTable);
+
+    // let queryBuilderRules = ;
+
+
+    $('#task-queue-update-button').click(function() {
+      var params = {};
+      params["task_queue"] = {}
+      // params["task_queue"]["query_builder_rules"] = $('#builder').queryBuilder('getRules');
+      params["task_queue"]["query_builder_rules"] = JSON.stringify($('#builder').queryBuilder('getRules'), null, 2);
+      // params["task_queue"]["details"] = document.getElementById('task_queue_details').value;
+      // params["task_queue"]["table"] = document.getElementById('task_queue_table').value;
+
+      // document.getElementById("queue-builder-modal-save-button").disabled = true;
+
+      $.ajax({
+        url: "/task_queues/" + taskQueueId,
+        type: 'PATCH',
+        data: params,
+        dataType: "json",
+        error: function(XMLHttpRequest, errorTextStatus, error) {
+          console.log("Failed: "+ errorTextStatus+" ;"+error);
+        },
+        success: function(response, status, request) {
+          console.log("it worked");
+          // debugger
+          let rows = response.rows
+          let columns = response.columns
+
+          loadTaskQueuePreview(columns, rows);
+          // $('#builder').queryBuilder('setRules', param_1, param_2);
+        }
+      })
+      // updateTaskQueue(params);
+    })
+  }
+}
 
 function getFieldsWithType(table) {
   $.ajax({
@@ -61,7 +129,7 @@ function loadQueryBuider(data) {
     var id = data[i][0]
     filter['id'] = id
 
-    if (data[i][1] === "inet") {
+    if (data[i][1] === "inet" || data[i][1] === "text") {
       type = "string"
     } else {
       type = data[i][1]
@@ -74,5 +142,30 @@ function loadQueryBuider(data) {
   $('#builder').queryBuilder({
     filters: filters,
     allow_groups: false
+  });
+}
+
+function saveTaskQueue(params) {
+  // document.getElementById("queue-builder-modal-save-button").disabled = true;
+
+  $.ajax({
+    url: "/task_queues",
+    type: 'POST',
+    data: params,
+    dataType: "json",
+    error: function(XMLHttpRequest, errorTextStatus, error) {
+      console.log("Failed: "+ errorTextStatus+" ;"+error);
+    },
+    success: function(response, status, request) {
+      console.log("it worked");
+    }
+  })
+}
+
+function loadTaskQueuePreview(columns, rows) {
+  // debugger
+  $('.task-queue-preview-table').footable({
+    "columns": columns,
+    "rows": rows
   });
 }
