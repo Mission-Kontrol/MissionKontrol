9c8bc8f495c16171b997644b1f394d62668cda78
Tomasz Skibinski <tomasz.skibinski@lcloud.pl>
2019-04-26 16:50:23 +0200

    Basic configuration for nginx web server

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -24,13 +24,30 @@ if [[ $? == 0 ]]; then
 fi
 echo
 
+# Run WebServer?
+if [[ "${WEB_SERVER_UP:-false}" = "true" ]]; then
+    export APP_SERVER_ADDR=127.0.0.1
+    export APP_SERVER_PORT=3001
+
+    echo "==> Starting nginx WebServer. <=="
+    dockerize \
+        -template /app/config/nginx_default.tmpl:/etc/nginx/conf.d/default.conf \
+        -stdout /var/log/nginx/access.log \
+        -stderr /var/log/nginx/error.log \
+        nginx
+    echo
+
+    # Run Puma as application server.
+    set -- bundle exec puma -b tcp://$APP_SERVER_ADDR:$APP_SERVER_PORT --pidfile /tmp/server.pid
+fi
+
 # If CMD starts with an option, prepend 'rails server'.
 [[ "${1:0:1}" = '-' ]] \
     && set -- bundle exec rails server "$@"
 
 # Set a default CMD, in case it wasn't provided.
 [[ -z "$1" ]] \
-    && set -- bundle exec rails server -p 3000 -b 0.0.0.0 "$@"
+    && set -- bundle exec rails server -p ${APP_SERVER_PORT:-3000} -b ${APP_SERVER_ADDR:-0.0.0.0} "$@"
 
 # If the command to execute is 'rails server', then force it to write the pid file
 # into a non-shared container directory. Suddenly killing and removing app containers
@@ -41,4 +58,3 @@ echo
 
 echo "==> Executing container start up command: \"$@\" <=="
 exec "$@"
-
