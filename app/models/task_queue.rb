3250d9293c49a95047338341fbf8abeec64618a8
Joshua Ob <joshuaaob@gmail.com>
2019-04-26 16:43:11 +0100

    Add method to convert rules to SQL

--- app/models/task_queue.rb
+++ app/models/task_queue.rb
@@ -1,50 +1,18 @@
 # frozen_string_literal: true
 
 class TaskQueue < ApplicationRecord
-  # FAILURE_TIMEOUT_INTERVALS = [
-  #   ['1 hour', 1],
-  #   ['2 hours', 2],
-  #   ['3 hours', 3]
-  # ].freeze
-
   validates :name, presence: true
-  # after_initialize :add_default_filter,
-  #                  :add_default_outcome
 
-  # def to_sql
-  #   result = ''
-  #   result + generate_db_select_sql + ' ' + generate_filters_sql + ';'
-  # end
-  #
-  # def sql_to_run
-  #   sql_query.present? ? sql_query : to_sql
-  # end
-  #
-  # private
-  #
-  # def generate_db_select_sql
-  #   "SELECT * FROM #{data_table_name}"
-  # end
-  #
-  # def generate_filters_sql
-  #   result = ''
-  #   sql_filters.each do |filter_attributes|
-  #     sql_filter = filter_attributes['sql_filter']
-  #     filter = SQLFilterFactory.build_sql_filter(sql_filter)
-  #     result += filter.to_sql
-  #   end
-  #   result
-  # end
-  #
-  # def add_default_filter
-  #   return if sql_filters.present?
-  #   filter = SQLFilter::Equal.new
-  #   sql_filters << { 'sql_filter' => filter.to_hash }
-  # end
-  #
-  # def add_default_outcome
-  #   return if outcomes.present?
-  #   outcome = WorkListOutcome.new.to_hash
-  #   outcomes << { 'outcome' => outcome }
-  # end
+  def to_sql
+    result = ""
+    query_builder = JSON.parse(query_builder_rules)
+
+    if query_builder["rules"].size > 1
+      query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"], condition: query_builder["condition"])
+    else
+      query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"])
+    end
+
+    result += "select * from #{table} #{query_builder_parser.to_sql}"
+  end
 end
