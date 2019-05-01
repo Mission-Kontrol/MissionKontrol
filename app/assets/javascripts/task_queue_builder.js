6befe1c90ff0ca2c310d85a322c4a79b19a416a5
Joshua Ob <joshuaaob@gmail.com>
2019-05-01 17:02:23 +0100

    Improve code quality

--- app/assets/javascripts/task_queue_builder.js
+++ app/assets/javascripts/task_queue_builder.js
@@ -31,9 +31,9 @@ function loadIndexPage() {
       $("#new-queue-modal-screen-2").toggleClass("hide");
     })
 
-    $('#queue-builder-modal-save-button').click(function() {
+    $("#queue-builder-modal-save-button").click(function() {
       var params = {};
-      params["task_queue"] = {}
+      params["task_queue"] = {};
       params["task_queue"]["name"] = document.getElementById('task_queue_name').value;
       params["task_queue"]["details"] = document.getElementById('task_queue_details').value;
       params["task_queue"]["table"] = document.getElementById('task_queue_table').value;
@@ -49,26 +49,26 @@ function loadEditPage() {
 
     getFieldsWithType(taskQueueTable);
 
-    $('.task-queue-update-button').click(function() {
+    $(".task-queue-update-button").click(function() {
       var params = {};
       params["task_queue"] = {}
-      params["task_queue"]["query_builder_rules"] = JSON.stringify($('#builder').queryBuilder('getRules'), null, 2);
-      params["task_queue"]["raw_sql"] = document.getElementById('task_queue_raw_sql').value;;
+      params["task_queue"]["query_builder_rules"] = JSON.stringify($('#builder').queryBuilder("getRules"), null, 2);
+      params["task_queue"]["raw_sql"] = document.getElementById("task_queue_raw_sql").value;
 
       $.ajax({
         url: "/task_queues/" + taskQueueId,
-        type: 'PATCH',
+        type: "PATCH",
         data: params,
         dataType: "json",
         error: function(response, status, request) {
           console.error(status);
-          toastr.error('Task queue preview failed, review SQL.');
+          toastr.error("Task queue preview failed, review SQL.");
         },
         success: function(response, status, request) {
           let rows = response.rows
           let columns = response.columns
           loadTaskQueuePreview(columns, rows);
-          toastr.info('Task queue updated.');
+          toastr.info("Task queue updated.");
         }
       })
     })
@@ -78,7 +78,7 @@ function loadEditPage() {
 function getFieldsWithType(table) {
   $.ajax({
     url: "/layouts/table_fields_with_type",
-    type: 'GET',
+    type: "GET",
     data: {
       table: table
     },
@@ -109,37 +109,37 @@ function loadQueryBuider(data) {
       type = data[i][1]
     }
 
-    filter['type'] = type
+    filter["type"] = type
     filters.push(filter)
   }
 
-  $('#builder').queryBuilder({
+  $("#builder").queryBuilder({
     filters: filters,
     allow_groups: false,
-    operators: ['equal',
-                'not_equal',
-                'contains',
-                'not_contains',
-                'between',
-                'not_between',
-                'is_null',
-                'is_not_null',
-                'begins_with',
-                'not_begins_with',
-                'is_empty',
-                'is_not_empty',
-                'less',
-                'less_or_equal',
-                'greater',
-                'greater_or_equal',
-                'ends_with',
-                'not_ends_with']
+    operators: ["equal",
+                "not_equal",
+                "contains",
+                "not_contains",
+                "between",
+                "not_between",
+                "is_null",
+                "is_not_null",
+                "begins_with",
+                "not_begins_with",
+                "is_empty",
+                "is_not_empty",
+                "less",
+                "less_or_equal",
+                "greater",
+                "greater_or_equal",
+                "ends_with",
+                "not_ends_with"]
   });
 
   taskQueueRules = $("#builder").data().taskQueueRules;
 
   if (taskQueueRules) {
-    $("#builder").queryBuilder('setRules', taskQueueRules)
+    $("#builder").queryBuilder("setRules", taskQueueRules)
   }
 }
 
@@ -155,7 +155,6 @@ function saveTaskQueue(params) {
       console.error(response);
       console.error(status);
       console.error(request);
-      // console.log("Failed: "+ errorTextStatus+" ;"+error);
     },
     success: function(response, status, request) {
       console.log("it worked");
@@ -164,7 +163,7 @@ function saveTaskQueue(params) {
 }
 
 function loadTaskQueuePreview(columns, rows) {
-  $('.task-queue-preview-table').footable({
+  $(".task-queue-preview-table").footable({
     "columns": columns,
     "rows": rows
   });
