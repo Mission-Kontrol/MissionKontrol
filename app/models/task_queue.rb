b0943633c0eb268840965ba82200615ea9bc1438
Joshua Ob <joshuaaob@gmail.com>
2019-05-13 17:32:54 +0100

    Convert query builder sql into task queue sql

--- app/models/task_queue.rb
+++ app/models/task_queue.rb
@@ -5,15 +5,23 @@ class TaskQueue < ApplicationRecord
 
   def to_sql
     result = ""
-    return result if query_builder_rules.blank?
-    query_builder = JSON.parse(query_builder_rules)
-
-    if query_builder["rules"].size > 1
-      query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"], condition: query_builder["condition"])
-    else
-      query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"])
-    end
-
-    result += "select * from #{table} #{query_builder_parser.to_sql}"
+    result += "select * from #{table} where #{query_builder_sql};" unless query_builder_sql.blank?
+    result
   end
+
+  # def to_sql
+  #   result = ""
+    # return result if query_builder_rules.blank?
+
+    # query_builder = JSON.parse(query_builder_rules)
+    #
+    # if query_builder["rules"].size > 1
+    #   query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"], condition: query_builder["condition"])
+    # else
+    #   query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"])
+    # end
+
+  #   result += "select * from #{table} where #{query_builder_sql};" if query_builder_sql
+  #   result
+  # end
 end
