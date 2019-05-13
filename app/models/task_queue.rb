1df55d5aea24b73179227ee0ac2514ac97b9d119
Joshua Ob <joshuaaob@gmail.com>
2019-05-13 18:10:03 +0100

    Improve code quality

--- app/models/task_queue.rb
+++ app/models/task_queue.rb
@@ -4,24 +4,7 @@ class TaskQueue < ApplicationRecord
   validates :name, :table, presence: true
 
   def to_sql
-    result = ""
-    result += "select * from #{table} where #{query_builder_sql};" unless query_builder_sql.blank?
-    result
+    return "" if query_builder_sql.blank?
+    "select * from #{table} where #{query_builder_sql};"
   end
-
-  # def to_sql
-  #   result = ""
-    # return result if query_builder_rules.blank?
-
-    # query_builder = JSON.parse(query_builder_rules)
-    #
-    # if query_builder["rules"].size > 1
-    #   query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"], condition: query_builder["condition"])
-    # else
-    #   query_builder_parser = QueryBuilderParser.new(rules: query_builder["rules"])
-    # end
-
-  #   result += "select * from #{table} where #{query_builder_sql};" if query_builder_sql
-  #   result
-  # end
 end
