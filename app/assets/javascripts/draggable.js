834c96656c06605527710513b390b45166c42199
Joshua Ob <joshuaaob@gmail.com>
2019-05-17 16:27:56 +0100

    Extract draggable functionality into its own file

---
+++ app/assets/javascripts/draggable.js
@@ -0,0 +1,49 @@
+function initializeDraggable() {
+  const containers = '#layout-builder-draggable-trash-container, #layout-builder-draggable-fields-container, #layout-builder-draggable-header-container1, #layout-builder-draggable-header-container2, #layout-builder-draggable-side-container, #layout-builder-draggable-main-container1, #layout-builder-draggable-main-container2, #layout-builder-draggable-main-container3, #task-queue-draggable-field-settings-container'
+  const dataContainers = '#layout-builder-draggable-trash-container, #layout-builder-draggable-header-container1, #layout-builder-draggable-header-container2, #layout-builder-draggable-side-container, #layout-builder-draggable-main-container1, #layout-builder-draggable-main-container2, #layout-builder-draggable-main-container3, #task-queue-draggable-field-settings-container'
+
+  draggable = new window.Draggable.Sortable(document.querySelectorAll(containers), {
+    draggable: '.layout-builder-draggable-item',
+    handle: '.layout-builder-draggable-item-handle'
+  });
+
+  const fieldsContainer = document.querySelectorAll('#layout-builder-draggable-fields-container')[0];
+
+  draggable.on('drag:start', (dragEvent) => {
+    showTrashContainer();
+  })
+
+  draggable.on('drag:stop', (dragEvent) => {
+    let currentContainer = dragEvent.source.parentNode;
+    let destinationContainerId = currentContainer.id;
+    let sourceContainerId = dragEvent.data.sourceContainer.id;
+    let currentContainerId = currentContainer.id;
+    let currentFieldValue = dragEvent.source.innerText.trim();
+
+    hideTrashContainer();
+
+    if (destinationContainerId === 'layout-builder-draggable-trash-container') {
+      fieldsContainer.insertBefore(dragEvent.source, fieldsContainer.childNodes[0]);
+
+      setTimeout(function () {
+        fieldsContainer.firstElementChild.classList.toggle('layout-builder-trash-can-item-put-back');
+      }, 100);
+
+      setTimeout(function () {
+        fieldsContainer.firstElementChild.classList.toggle('layout-builder-trash-can-item-put-back');
+      }, 2000);
+    }
+
+    if (sourceContainerId === destinationContainerId) {
+      return
+    }
+
+    if (isDataContainer(sourceContainerId)) {
+      saveDraggableContainer(dragEvent, sourceContainerId)
+    }
+
+    if (isDataContainer(destinationContainerId)) {
+      saveDraggableContainer(dragEvent, destinationContainerId)
+    }
+  });
+}
