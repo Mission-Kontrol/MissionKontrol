2f4ae9dbf8d4cc77a009995301f6f6a5588fa1fe
Joshua Ob <joshuaaob@gmail.com>
2019-05-21 08:07:19 +0100

    Add eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+{
+  "globals": [
+    "initializeDraggable": "readonly",
+    "updateDraggableFieldsContainer": "readonly",
+    "updateLayoutBuilderContainer": "readonly",
+    "isNotFieldsContainer": "readonly",
+    "updateTaskQueueDraggableFields": "readonly",
+    "updateTaskQueueDraggableFields": "readonly",
+    "getContainerItemsJSON": "readonly"
+  ]
+}
