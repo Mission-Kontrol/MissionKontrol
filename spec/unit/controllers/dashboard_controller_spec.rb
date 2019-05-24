026a59b16840564f39146178b8ee09a899c512e6
Joshua Ob <joshuaaob@gmail.com>
2019-05-24 16:24:20 +0100

    Add spec for license check when viewing the dashboard

--- spec/unit/controllers/dashboard_controller_spec.rb
+++ spec/unit/controllers/dashboard_controller_spec.rb
@@ -12,7 +12,10 @@ describe DashboardController, :type => :controller do
       it "renders the bad connection template" do
         sign_in admin
         allow(controller).to receive(:show).and_raise(InvalidClientDatabaseError.new)
-        get :show
+
+        VCR.use_cassette('license_check_success') do
+          get :show
+        end
 
         expect(response).to render_template("tables/bad_connection")
       end
@@ -21,8 +24,25 @@ describe DashboardController, :type => :controller do
     context "when client database connection is valid" do
       it "renders the show template" do
         sign_in admin
-        get :show
+
+        VCR.use_cassette('license_check_success') do
+          get :show
+        end
+
         expect(response).to render_template("show")
+      end
+    end
+
+    context "when admin user has not activated license" do
+      it "redirects to the license route" do
+        user = create(:admin_user)
+        sign_in user
+
+        VCR.use_cassette('license_check_failure') do
+          get :show
+        end
+
+        expect(response).to redirect_to(license_path)
       end
     end
   end
