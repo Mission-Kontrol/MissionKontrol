2b459f4a687b74b5798122c80adb8915869fdf79
Joshua Ob <joshuaaob@gmail.com>
2019-05-25 14:35:03 +0100

    Update sign in helper to include license

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -2,11 +2,11 @@
 
 module Features
   module SessionHelpers
-    def sign_in_as_admin
-      user = AdminUser.find_by_email('test@kuwinda.io') || AdminUser.create(email: 'test@kuwinda.io', password: '123456', password_confirmation: '123456')
+    def sign_in_as_admin_with_license
+      user = create(:admin_user, :with_license)
       visit root_path
       fill_in 'Email', with: user.email
-      fill_in 'Password', with: '123456'
+      fill_in 'Password', with: 'password'
       click_button 'Log in'
     end
   end
