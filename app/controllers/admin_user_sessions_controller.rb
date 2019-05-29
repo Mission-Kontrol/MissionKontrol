fb05c13de8097b3ea69410f756021c905ec773be
Joshua Ob <joshuaaob@gmail.com>
2019-05-29 14:50:37 +0100

    Use the new Ruby 1.9 hash syntax

--- app/controllers/admin_user_sessions_controller.rb
+++ app/controllers/admin_user_sessions_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 class AdminUserSessionsController < Devise::SessionsController
-  skip_before_action :check_license, :only => %i[new]
+  skip_before_action :check_license, only: %i[new]
 end
