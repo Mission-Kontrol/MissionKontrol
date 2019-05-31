e5738052ea2a39f4663b3b1648605e8942f28de3
Joshua Ob <joshuaaob@gmail.com>
2019-05-31 14:50:17 +0100

    Add methods to activate and validate licenses as full or trial

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -16,16 +16,33 @@ class DashboardController < ApplicationController
 
   def verify_license
     current_admin_user.license_key = params[:license_key] if params[:license_key]
-    license_active = activate_current_license
-    license_valid = validate_current_license if license_active
-    current_admin_user.save if license_valid
-    redirect_to dashboard_path if license_valid
+
+    if activate_current_license && validate_current_license
+      current_admin_user.save
+      redirect_to dashboard_path
+    else
+      render 'verify_license'
+    end
   end
 
   private
 
   def activate_current_license
-    activate_result = activate_license
+    return false unless current_admin_user
+    return true if activate_full_license
+    return true if activate_trial_license
+    false
+  end
+
+  def validate_current_license
+    return false unless current_admin_user
+    return true if validate_full_license
+    return true if validate_trial_license
+    false
+  end
+
+  def activate_full_license
+    activate_result = VerifyLicenseKeyService.activate_full(current_admin_user)
 
     if activate_result[:status] == 200
       current_admin_user.activation_id = activate_result[:data]['activation_id']
@@ -35,8 +52,25 @@ class DashboardController < ApplicationController
     end
   end
 
-  def validate_current_license
-    validate_license_key[:status] == 200
+  def activate_trial_license
+    activate_result = VerifyLicenseKeyService.activate_trial(current_admin_user)
+
+    if activate_result[:status] == 200
+      current_admin_user.activation_id = activate_result[:data]['activation_id']
+      true
+    else
+      false
+    end
+  end
+
+  def validate_full_license
+    validate_result = VerifyLicenseKeyService.validate_full(current_admin_user)
+    validate_result[:status] == 200 ? true : false
+  end
+
+  def validate_trial_license
+    validate_result = VerifyLicenseKeyService.validate_trial(current_admin_user)
+    validate_result[:status] == 200 ? true : false
   end
 
   def load_admin_db_config
