df7756230d159c3b6b7d1011ab42faeffb59177d
Joshua Ob <joshuaaob@gmail.com>
2019-06-01 09:55:00 +0100

    Extract license related methods to concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class InvalidClientDatabaseError < StandardError; end
 
 class ApplicationController < ActionController::Base
+  include License
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -137,87 +138,5 @@ class ApplicationController < ActionController::Base
     SensitiveData.set_target_database_credential(:database_host, uri.host)
     SensitiveData.set_target_database_credential(:database_port, uri.port)
     SensitiveData.set_target_database_credential(:database_type, 'postgres')
-  end
-
-  def activate_current_license
-    return false unless current_admin_user
-
-    return true if activate_full_license || activate_trial_license
-
-    false
-  end
-
-  def validate_current_license
-    return false unless current_admin_user
-
-    return true if validate_full_license || validate_trial_license
-
-    false
-  end
-
-  def activate_full_license
-    activate_result = VerifyLicenseKeyService.activate_full(current_admin_user)
-
-    if activate_result[:status] == 200
-      current_admin_user.activation_id = activate_result[:data]['activation_id']
-      true
-    else
-      false
-    end
-  end
-
-  def activate_trial_license
-    activate_result = VerifyLicenseKeyService.activate_trial(current_admin_user)
-
-    if activate_result[:status] == 200
-      current_admin_user.activation_id = activate_result[:data]['activation_id']
-      true
-    else
-      false
-    end
-  end
-
-  def validate_full_license
-    validate_result = VerifyLicenseKeyService.validate_full(current_admin_user)
-
-    if validate_result[:status] == 200
-      current_admin_user.full_license = true
-      true
-    else
-      false
-    end
-  end
-
-  def validate_trial_license
-    validate_result = VerifyLicenseKeyService.validate_trial(current_admin_user)
-    validate_result[:status] == 200
-  end
-
-  def license_verified
-    activate_current_license && validate_current_license
-  end
-
-  def fetch_license_cache(cache_key)
-    Rails.cache.fetch(cache_key)
-  end
-
-  def license_cache=(cache_key)
-    Rails.cache.fetch(cache_key, expires_in: 24.hours) { cache_key } if license_verified
-  end
-
-  def license_valid?
-    return false unless current_admin_user
-
-    cache_key = "license-#{current_admin_user.license_key}"
-    license_cache = fetch_license_cache(cache_key)
-
-    if license_cache
-      true
-    elsif license_verified
-      license_cache = cache_key
-      true
-    else
-      false
-    end
   end
 end
