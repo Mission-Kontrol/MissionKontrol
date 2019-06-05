09f2f097589b339a47a2dfa215830b93e56cf75d
Joshua Ob <joshuaaob@gmail.com>
2019-06-05 18:02:45 +0100

    Update outcome timeout options

--- app/models/task_queue.rb
+++ app/models/task_queue.rb
@@ -4,7 +4,11 @@ class TaskQueue < ApplicationRecord
   validates :name, :table, presence: true
 
   OUTCOME_TIMEOUTS = [
-    ['1 Day', '86400']
+    ['1 Day', '1'],
+    ['5 Days', '5'],
+    ['1 week', '7'],
+    ['2 weeks', '14'],
+    ['1 month', '30']
   ].freeze
 
   def to_sql
