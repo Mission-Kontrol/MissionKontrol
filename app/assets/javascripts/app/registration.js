2e38d48331a77f42df29aaabddeb7c728e4c0303
Joshua Ob <joshuaaob@gmail.com>
2019-06-11 16:46:45 +0100

    Add js for registration page

---
+++ app/assets/javascripts/app/registration.js
@@ -0,0 +1,15 @@
+// $(document).ready(function() {
+//
+// })
+
+function goToRegistrationStep(step) {
+  $('#registration-step-1, #registration-step-2').toggleClass('hide');
+  
+  // if (step === 2) {
+  //   $('#registration-step-1, #registration-step-2').toggleClass('hide');
+  // }
+  //
+  // if (step === 2) {
+  //   $('#registration-step-1, #registration-step-2').toggleClass('hide');
+  // }
+}
