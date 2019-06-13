bd74dd7ccefd61b8b3c1380eccb13340af9d70fe
Joshua Ob <joshuaaob@gmail.com>
2019-06-13 14:22:41 +0100

    Cleanup

--- app/assets/javascripts/app/registration.js
+++ app/assets/javascripts/app/registration.js
@@ -1,15 +1,3 @@
-// $(document).ready(function() {
-//
-// })
-
 function goToRegistrationStep(step) {
   $('#registration-step-1, #registration-step-2').toggleClass('hide');
-  
-  // if (step === 2) {
-  //   $('#registration-step-1, #registration-step-2').toggleClass('hide');
-  // }
-  //
-  // if (step === 2) {
-  //   $('#registration-step-1, #registration-step-2').toggleClass('hide');
-  // }
 }
