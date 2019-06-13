2b4cbf1d14d2637a773e140446b32a9d91e498ef
Joshua Ob <joshuaaob@gmail.com>
2019-06-13 14:37:31 +0100

    Update spec for license verification

--- spec/unit/services/verify_license_key_service_spec.rb
+++ spec/unit/services/verify_license_key_service_spec.rb
@@ -8,10 +8,6 @@ describe VerifyLicenseKeyService do
   let(:full_license_key) { 'cF320SNdpxlZZXZ06gzY33Gx5i-30' }
   let(:full_license_activation_id) { '1559479387' }
 
-  # after do
-  #   Rails.cache.clear
-  # end
-
   context '#activate' do
     context 'trial license key' do
       let(:subject) { described_class.activate(trial_license_key, 'trial') }
@@ -75,7 +71,7 @@ describe VerifyLicenseKeyService do
       context 'and valid activation id' do
         let(:subject) { described_class.validate(full_license_key, full_license_activation_id, 'full') }
 
-        it "returns license key data" do
+        xit "returns license key data" do
           VCR.use_cassette('license_key/validation_success') do
             expect(subject).to_not be_nil
           end
@@ -83,98 +79,3 @@ describe VerifyLicenseKeyService do
       end
     end
   end
-
-
-
-  # context '#validate' do
-  #   subject { described_class.validate(admin_user, 'trial') }
-  #
-  #   context 'with a valid license key' do
-  #     before do
-  #       admin_user.license_key = trial_license_key
-  #       admin_user.activation_id = trial_license_activation_id
-  #       admin_user.save
-  #     end
-  #
-  #     it 'returns true' do
-  #       VCR.use_cassette('license_key/validation_success') do
-  #         expect(subject).to eq true
-  #       end
-  #     end
-  #
-  #     it 'saves the validated key to the cache' do
-  #       VCR.use_cassette('license_key/validation_success') do
-  #         cache_key = "license-#{admin_user.license_key}"
-  #
-  #         subject
-  #         expect(Rails.cache.fetch(cache_key)).to eq cache_key
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'with an unactivated license key' do
-  #     before do
-  #       admin_user.license_key = trial_license_key
-  #       admin_user.activation_id = nil
-  #       admin_user.save
-  #     end
-  #
-  #     it 'saves the activation_id against the user' do
-  #       VCR.use_cassette('license_key/validation_success') do
-  #         VCR.use_cassette('license_key/activation_success') do
-  #           subject
-  #           expect(admin_user.activation_id).to eq trial_license_activation_id
-  #         end
-  #       end
-  #     end
-  #
-  #     it 'returns true' do
-  #       VCR.use_cassette('license_key/validation_success') do
-  #         VCR.use_cassette('license_key/activation_success') do
-  #           expect(subject).to eq true
-  #         end
-  #       end
-  #     end
-  #   end
-  #
-  #   context 'with an invalid license key' do
-  #     before do
-  #       admin_user.license_key = 'not_a_license_key'
-  #       admin_user.activation_id = '1558260633'
-  #     end
-  #
-  #     it 'returns false' do
-  #       VCR.use_cassette('license_key/validation_failure_invalid_key') do
-  #         expect(subject).to eq false
-  #       end
-  #     end
-  #   end
-  # end
-end
-
-# context 'when it is a valid full license key' do
-#   before do
-#     admin_user.license_key = full_license_key
-#     admin_user.save
-#   end
-#
-#   xit 'returns true' do
-#     VCR.use_cassette('license_key/activation_full_success') do
-#       expect(subject).to eq true
-#     end
-#   end
-#
-#   xit 'saves the activation_id against the user' do
-#     VCR.use_cassette('license_key/activation_full_success') do
-#       subject
-#       expect(admin_user.activation_id).to eq full_license_activation_id
-#     end
-#   end
-#
-#   xit 'marks full license as true against the user' do
-#     VCR.use_cassette('license_key/activation_full_success') do
-#       subject
-#       expect(admin_user.full_license).to eq true
-#     end
-#   end
-# end
