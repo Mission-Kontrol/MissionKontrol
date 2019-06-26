356ee894259a76973532fbb673422589da5e711a
Joshua Ob <joshuaaob@gmail.com>
2019-06-26 17:09:25 +0100

    Add href to row when added to DOM

--- app/assets/javascripts/layout_builder.js
+++ app/assets/javascripts/layout_builder.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   prepareNormalToast();
   addPaddingToDraggableItems(containers);
 
-  $(".clickable-row").click(function() {
+  $("body").on('click', '.clickable-row' , function() {
       window.location = $(this).data("href");
   });
 
