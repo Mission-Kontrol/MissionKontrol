87ebec743fa62b62ed2f15ec8c0741c67dac2683
Joshua Ob <joshuaaob@gmail.com>
2019-06-28 13:49:14 +0100

    Add model for data table state

---
+++ app/models/data_table_state.rb
@@ -0,0 +1,21 @@
+class DataTableState < ApplicationRecord
+  def state_as_json
+    columns = []
+
+    state["columns"].each do |k, v|
+      val = {}
+      val["visible"] = ActiveModel::Type::Boolean.new.cast(v["visible"])
+      val["search"] = v["search"]
+      columns << val
+    end
+
+    {
+      time: state["time"].to_i,
+      start: state["start"].to_i,
+      length: state["length"].to_i,
+      order: state["order"],
+      search: state["search"],
+      columns: columns
+    }
+  end
+end
