ea517b7afdb225b42c25abfcf301f7765f89767f
Camilla van Klinken <camillavk@gmail.com>
2019-09-01 15:56:33 +0100

    Save org settings on change

--- app/controllers/organisation_settings_controller.rb
+++ app/controllers/organisation_settings_controller.rb
@@ -8,4 +8,20 @@ class OrganisationSettingsController < ApplicationController
   def edit
     @organisation = OrganisationSetting.find params[:id]
   end
+
+  def update
+    @organisation = OrganisationSetting.find params[:id]
+
+    if @organisation.update!(organisation_params)
+      respond_to do |format|
+        format.js { }
+      end
+    end
+  end
+
+  private
+
+  def organisation_params
+    params.require(:organisation_setting).permit(:company_name, :license_key)
+  end
 end
