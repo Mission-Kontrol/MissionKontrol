c46a06c1fb6a692ca02ba64c75195e9ec0a1aa04
Camilla van Klinken <camillavk@gmail.com>
2019-11-10 12:13:47 +0000

    Don't load everything when loading modal

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -7,7 +7,7 @@ class AdminUsersController < ApplicationController
 
   before_action :load_available_tables,
                 :load_task_queues,
-                :check_target_db_connection
+                :check_target_db_connection, only: [:index]
 
   def index
     @users = AdminUser.all
