5bfdbb477059990ce89eee568ed336a18e0c3797
Camilla van Klinken <camillavk@gmail.com>
2019-12-29 14:51:35 +0000

    fix codacy

--- app/assets/javascripts/app/components/modal.js
+++ app/assets/javascripts/app/components/modal.js
@@ -2,7 +2,7 @@
 
 function closeModal() {
   $("body").on("click", ".modal--close-btn", function () {
-        var modal = $(this).parents('.show');
+        var modal = $(this).parents(".show");
         modal.removeClass("show");
         modal.addClass("hide");
     });
