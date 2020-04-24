14526d61963c89efdb93127da8043fc4e66a67a3
Camilla <camillavk@gmail.com>
2020-04-24 13:22:34 +0100

    Temporarily give up on testing draggable

--- spec/features/single_data_view/dragging_primary_fields_to_container_spec.rb
+++ spec/features/single_data_view/dragging_primary_fields_to_container_spec.rb
@@ -1,42 +1,42 @@
-# dragging to container
-# dragging out of container (and refreshing?)
-# dragging into trash
+# # dragging to container
+# # dragging out of container (and refreshing?)
+# # dragging into trash
 
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-feature 'Dragging primary fields to container', js: true do
-  background do
-    sign_in_as_admin_with_license
+# feature 'Dragging primary fields to container', js: true do
+#   background do
+#     sign_in_as_admin_with_license
 
-    setup_tables_and_roles('events')
-    @view_builder = ViewBuilder.create(table_name: 'events', database_id: @database.id)
-    visit edit_layout_path(@view_builder.id)
-    find('.sv_builder_table_navigation').click
-  end
+#     setup_tables_and_roles('events')
+#     @view_builder = ViewBuilder.create(table_name: 'events', database_id: @database.id)
+#     visit edit_layout_path(@view_builder.id)
+#     find('.sv_builder_table_navigation').click
+#   end
 
-  scenario 'from field list into view container' do
-    source = first('.layout-builder-draggable-item')
-    destination = find('#layout-builder-draggable-main-container3')
-    source.drag_to destination
-    sleep 15
-    binding.pry
-  end
+#   scenario 'from field list into view container' do
+#     source = first('.layout-builder-draggable-item')
+#     destination = find('#layout-builder-draggable-main-container3')
+#     source.drag_to destination
+#     sleep 15
+#     binding.pry
+#   end
 
-  scenario 'from view container into field list' do
-  end
+#   scenario 'from view container into field list' do
+#   end
 
-  scenario 'from view container into field list when list closed' do
-  end
+#   scenario 'from view container into field list when list closed' do
+#   end
 
-  scenario 'from field list into trash can' do
-  end
+#   scenario 'from field list into trash can' do
+#   end
 
-  scenario 'from view container into trash can' do
-  end
+#   scenario 'from view container into trash can' do
+#   end
 
-  scenario 'from view container into trash can when list closed' do
-  end
+#   scenario 'from view container into trash can when list closed' do
+#   end
 
-  scenario 'creates a new layout with primary table fields visible' do
-  end
-end
+#   scenario 'creates a new layout with primary table fields visible' do
+#   end
+# end
