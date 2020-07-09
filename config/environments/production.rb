e78c6c7681bd3acf5c9e6e51f835a553de641a48
Camilla <camillavk@gmail.com>
2020-07-09 20:07:29 +0100

    Remove rubyracer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,6 +23,8 @@ Rails.application.configure do
 
   config.read_encrypted_secrets = true
 
+  config.i18n.available_locales = %i(en)
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
